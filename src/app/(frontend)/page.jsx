c23dffb263b1bcb7121f623e826af51fbaafd073
Jitendra Kumar Bijarnia <67179546+img-website@users.noreply.github.com>
2025-09-09 10:05:17 +0530

    added sprite.svg

--- src/app/(frontend)/page.jsx
+++ src/app/(frontend)/page.jsx
@@ -1,10 +1,12 @@
 import Home from '@/components/frontend/home/Home'
+import Svg from '@/components/svg'
 import React from 'react'
 
 const page = () => {
   return (
     <>
       <Home/>
+      <Svg name="bankVerify" className="w-6 h-6 text-gray-500" />
     </>
   )
 }

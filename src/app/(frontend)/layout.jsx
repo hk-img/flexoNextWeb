70304ab8d6a24d37b0cc83408863cd5b2a2d78a9
Himanshu Gandhi <162088731+himanshu-img@users.noreply.github.com>
2025-09-11 14:52:22 +0530

    fix code

--- src/app/(frontend)/layout.jsx
+++ src/app/(frontend)/layout.jsx
@@ -1,18 +1,14 @@
 import Footer from "@/components/frontend/footer/Footer";
 import Header from "@/components/frontend/header/Header";
-import Call from "@/components/frontend/icons/Call";
 import ChatBot from "@/components/frontend/icons/ChatBot";
-import Whatsapp from "@/components/frontend/icons/Whatsapp";
 import React from "react";
 
 const layout = ({ children }) => {
   return (
     <>
       <Header />
-        <Call/>
-        <Whatsapp/>
-        <ChatBot/>
         {children}
+        <ChatBot/>
       <Footer />
     </>
   );

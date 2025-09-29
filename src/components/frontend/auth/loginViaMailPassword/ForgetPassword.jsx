9d04d5c9a557938c0dd6efffd24977aed3a335c0
Soniya <soniyasaini04.img@gmail.com>
2025-09-29 10:28:20 +0530

    auth popup design

--- src/components/frontend/auth/loginViaMailPassword/ForgetPassword.jsx
+++ src/components/frontend/auth/loginViaMailPassword/ForgetPassword.jsx
@@ -24,11 +24,12 @@ const ForgetPassword = ({setIsForgetPassword}) => {
   };
   return (
     <>
+    <div className="p-5">
       <button
         onClick={() => {
           setIsForgetPassword(false);
         }}
-        className="py-5 cursor-pointer flex items-center text-[#f76900] font-medium"
+        className="cursor-pointer flex items-center text-[#f76900] font-medium"
       >
         <Svg name="leftArrow" className="size-4" />
         Back
@@ -37,7 +38,7 @@ const ForgetPassword = ({setIsForgetPassword}) => {
         onSubmit={handleSubmit(onSubmit)}
         className="py-5 flex flex-col gap-4"
       >
-        <div>
+        {/* <div>
           <input
             type="email"
             placeholder="Email"
@@ -51,7 +52,16 @@ const ForgetPassword = ({setIsForgetPassword}) => {
           {errors.email && (
             <p className="text-red-500 text-sm mt-1">{errors.email.message}</p>
           )}
-        </div>
+        </div> */}
+         <div className="relative">
+            <input  {...register("email")}  type="text" id="Email" className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#3f51b5] peer" placeholder=" " />
+            <label for="Email" className="absolute text-sm font-semibold text-[#00000099] duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-[#3f51b5]   peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">Email*</label>
+            {errors.email && (
+            <p className="text-red-500 text-sm mt-1">
+              {errors.email.message}
+            </p>
+              )}
+            </div>
         <button
           type="submit"
           className="bg-[#f76900] text-white h-12 rounded-md font-semibold hover:bg-orange-600 transition"
@@ -59,6 +69,7 @@ const ForgetPassword = ({setIsForgetPassword}) => {
           GET OTP
         </button>
       </form>
+      </div>
     </>
   );
 };

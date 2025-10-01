14c29c47e5c76f48ed0c57b9b7eb9e9f9d0ac25d
Himanshu Gandhi <162088731+himanshu-img@users.noreply.github.com>
2025-10-01 15:48:18 +0530

    added code

--- src/components/frontend/listing/MapWithPrice.jsx
+++ src/components/frontend/listing/MapWithPrice.jsx
@@ -33,7 +33,7 @@ const calculateCenter = (markers) => {
   return { lat: centerLat, lng: centerLng };
 };
  
-export default function MapWithPrices({ spaceTypeSlug, spaces, hoveredSpaceId }) {
+export default function MapWithPrices({ type, spaces, hoveredSpaceId }) {
   const [selectedSpace, setSelectedSpace] = useState(null);
   const [center,setCenter] = useState(null);
   const { isLoaded } = useLoadScript({
@@ -56,7 +56,6 @@ useEffect(()=>{
             space.id === hoveredSpaceId ? "%23000000" : "%23ffffff";
  
         let price = 0;
-        const type = getTypeOfSpaceByWorkSpace(spaceTypeSlug || "");
         if (type === "coworking") {
           if (!space.flexible_desk_price) {
             price = space.privatecabin_price;

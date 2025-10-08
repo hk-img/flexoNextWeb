d6494e6a3966041648809e8ad911da94bed9f475
Soniya <90394991+SoniyaSaini@users.noreply.github.com>
2025-10-08 17:25:23 +0530

    my bookings page

--- src/components/frontend/myBooking/MyBooking.jsx
+++ src/components/frontend/myBooking/MyBooking.jsx
@@ -1,49 +1,117 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
 
 const MyBooking = () => {
+  const [activeTab, setActiveTab] = useState("tab1");
+
   return (
     <>
-      <div className='bg-[#f9f9f9]'>
+      <div className="bg-[#f9f9f9]">
         <div className="relative w-full lg:mt-[82px] sm:mt-[62px] mt-[63px]">
-          <div className="container mx-auto md:px-0 px-[15px] py-10">
-            <div className='flex'>
-              <div className="w-2/5">
-                 <div class="flex border-b border-gray-300">
-                    <input type="radio" name="tabs" id="tab1" class="hidden peer/tab1" checked/>
-                    <label for="tab1" class="flex-1 text-center py-3 cursor-pointer text-gray-600 font-medium border-b-2 border-transparent peer-checked/tab1:border-[#3f51b5] peer-checked/tab1:text-[#3f51b5] transition">
-                      Tab One
-                    </label>
+          <div className="container mx-auto md:px-0 px-[15px] pt-10">
+            <div className="flex flex-wrap">
+              <div className="md:w-2/5 w-full md:mb-0 mb-6">
+                <div className="flex md:flex-row flex-col bg-white rounded-[15px] overflow-hidden">
+                  {/* Tab 1 */}
+                  <button
+                    onClick={() => setActiveTab("tab1")}
+                    className={`flex-1 text-center py-[15px] text-[#777] text-sm 2xl:text-base cursor-pointer font-medium border-b-2 transition ${
+                      activeTab === "tab1"
+                        ? " border-[#f76900]"
+                        : " border-transparent"
+                    }`}
+                  >
+                    All Booking
+                  </button>
 
-                    <input type="radio" name="tabs" id="tab2" class="hidden peer/tab2"/>
-                    <label for="tab2" class="flex-1 text-center py-3 cursor-pointer text-gray-600 font-medium border-b-2 border-transparent peer-checked/tab2:border-[#3f51b5] peer-checked/tab2:text-[#3f51b5] transition">
-                      Tab Two
-                    </label>
+                  {/* Tab 2 */}
+                  <button
+                    onClick={() => setActiveTab("tab2")}
+                    className={`flex-1 text-center py-[15px] text-[#777] text-sm 2xl:text-base cursor-pointer font-medium border-b-2 transition ${
+                      activeTab === "tab2"
+                        ? " border-[#f76900]"
+                        : " border-transparent"
+                    }`}
+                  >
+                    Past Booking
+                  </button>
 
-                    <input type="radio" name="tabs" id="tab3" class="hidden peer/tab3"/>
-                    <label for="tab3" class="flex-1 text-center py-3 cursor-pointer text-gray-600 font-medium border-b-2 border-transparent peer-checked/tab3:border-[#3f51b5] peer-checked/tab3:text-[#3f51b5] transition">
-                      Tab Three
-                    </label>
+                  {/* Tab 3 */}
+                  <button
+                    onClick={() => setActiveTab("tab3")}
+                    className={`flex-1 text-center py-[15px] text-[#777] text-sm 2xl:text-base cursor-pointer font-medium border-b-2 transition ${
+                      activeTab === "tab3"
+                        ? " border-[#f76900]"
+                        : " border-transparent"
+                    }`}
+                  >
+                    Upcoming Booking
+                  </button>
+                </div>
+              </div>
+              <div className="md:w-3/5 w-full md:px-6 px-0">
+                <div className="flex md:flex-row flex-col  items-center gap-6">
+                  <div className="bg-white rounded-[15px] w-full">
+                    <input
+                        type="date"
+                        className=" w-full placeholder:text-[#777] placeholder:font-medium text-black mt-1 text-sm  rounded-sm focus:outline-none px-2 h-12"
+                      />
                   </div>
+                  <div className="bg-white rounded-[15px] px-2 w-full">
+                    <select
+                      className="w-full placeholder:text-[#777] text-black mt-1 text-sm rounded-sm focus:outline-none  h-12"
+                     
+                    >
+                      <option value="" >
+                        Select space type
+                      </option>
+                      <option value="2025-10-06">6 Oct 2025</option>
+                      <option value="2025-10-07">7 Oct 2025</option>
+                      <option value="2025-10-08">8 Oct 2025</option>
+                    </select>
+                  </div>
+                   <div className="bg-white rounded-[15px] px-2 w-full">
+                    <select
+                      className="w-full placeholder:text-[#777] text-black mt-1 text-sm rounded-sm focus:outline-none  h-12"
+                     
+                    >
+                      <option value="" >
+                        Select Booking Status
+                      </option>
+                      <option value="2025-10-06">6 Oct 2025</option>
+                      <option value="2025-10-07">7 Oct 2025</option>
+                      <option value="2025-10-08">8 Oct 2025</option>
+                    </select>
+                  </div>
+
+                </div>
               </div>
             </div>
-            <div>
-              <div class="mt-4">
-                <div class="hidden peer-checked/tab1:block">
-                  <p class="text-gray-700">This is content for Tab One.</p>
-                </div>
-                <div class="hidden peer-checked/tab2:block">
-                  <p class="text-gray-700">This is content for Tab Two.</p>
-                </div>
-                <div class="hidden peer-checked/tab3:block">
-                  <p class="text-gray-700">This is content for Tab Three.</p>
-                </div>
-              </div>
+            <div className="md:py-10 py-5">
+              <h3 className="md:text-2xl text-xl font-medium">Booking not found..</h3>
+            </div>
+            <div className="mt-4">
+              {activeTab === "tab1" && (
+                <p className="text-gray-700">
+
+                </p>
+              )}
+              {activeTab === "tab2" && (
+                <p className="text-gray-700">
+
+                </p>
+              )}
+              {activeTab === "tab3" && (
+                <p className="text-gray-700">
+
+                </p>
+              )}
             </div>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default MyBooking
+export default MyBooking;

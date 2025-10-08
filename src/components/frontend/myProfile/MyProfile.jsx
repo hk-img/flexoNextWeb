3bca36a272080d31a66f99c2828af37fa57ef188
Soniya <90394991+SoniyaSaini@users.noreply.github.com>
2025-10-08 12:58:11 +0530

    profile page

--- src/components/frontend/myProfile/MyProfile.jsx
+++ src/components/frontend/myProfile/MyProfile.jsx
@@ -1,8 +1,66 @@
+"use client";
+import React from 'react'
 import Svg from '@/components/svg'
 import Image from 'next/image'
-import React from 'react'
+import { useForm, Controller } from "react-hook-form";
+import { set, z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
+
+const profileSchema = z
+  .object({
+    firstName: z.string().min(1, "First Name is required"),
+    lastName: z.string().min(1, "Last Name is required"),
+    mobile: z.string().min(10, "Valid Mobile Number is required"),
+    country: z
+      .object({
+        dialCode: z.string().optional(),
+      })
+      .nullable()
+      .optional(),
+  })
+  .superRefine((data, ctx) => {
+    if (!data.mobile) {
+      ctx.addIssue({
+        path: ["mobile"],
+        message: "Mobile number is required",
+        code: z.ZodIssueCode.custom,
+      });
+    } else {
+      const code = data.country?.dialCode ?? "";
+      const numeric = data.mobile.replace(/\D/g, "");
+      if (numeric.length <= code.length) {
+        ctx.addIssue({
+          path: ["mobile"],
+          message: "Mobile number is required",
+          code: z.ZodIssueCode.custom,
+        });
+      }
+    }
+  });
 
 const MyProfile = () => {
+   const {
+      register,
+      handleSubmit,
+      setValue,
+      control,
+      formState: { errors },
+    } = useForm({
+      resolver: zodResolver(profileSchema),
+      defaultValues: {
+        firstName: "",
+        lastName: "",
+        mobile: "+91",
+        country: {
+          name: "India",
+          dialCode: "91",
+          countryCode: "in",
+          format: "+.. .....-.....",
+        },
+      },
+    });
   return (
     <>
       <div className='relative w-full lg:mt-[82px] sm:mt-[62px] mt-[63px]'>
@@ -19,14 +77,54 @@ const MyProfile = () => {
                   </div>
                   </div>
                 </div>
-                <div className='grid md:grid-cols-2 grid-cols-1 gap-4'>
+                <div className='mt-10'>
                   <div>
-                    <label htmlFor="first-name" className='text-sm 2xl:text-base font-semibold mb-1'>First name <span className='text-[#dc3545]'>*</span> </label>
-                    <input
-                      type="first-name"
-                      placeholder="Enter First Name"
-                      className={`h-12 px-4 border-2 border-[#e0e0e0] hover:border-black placeholder:text-sm rounded-md w-full focus:outline-none focus:border-[#001740]`}
+                    <h4 className='text-lg font-semibold 2xl:text-xl mb-5'> Basic Information </h4>
+                  </div>
+                  <div className='grid md:grid-cols-2 grid-cols-1 gap-6 '>
+                    <div>
+                      <label htmlFor="first-name" className='text-sm 2xl:text-base font-semibold'>First name <span className='text-[#dc3545]'>*</span> </label>
+                      <input
+                        type="first-name"
+                        placeholder="Enter First Name"
+                        className={`h-12 mt-1 px-4 border-2 border-[#e0e0e0] hover:border-black placeholder:text-sm rounded-[5px] w-full focus:outline-none focus:border-[#001740]`}
+                      />
+                    </div>
+                    <div>
+                      <label htmlFor="last-name" className='text-sm 2xl:text-base font-semibold'>last name <span className='text-[#dc3545]'>*</span> </label>
+                      <input
+                        type="last-name"
+                        placeholder="Enter last Name"
+                        className={`h-12 mt-1 px-4 border-2 border-[#e0e0e0] hover:border-black placeholder:text-sm rounded-[5px] w-full focus:outline-none focus:border-[#001740]`}
+                      />
+                    </div>
+                   <div className="relative">
+                    <Controller
+                      name="mobile"
+                      control={control}
+                      render={({ field }) => (
+                        <PhoneInput
+                          country="in"
+                          value={field.value}
+                          onChange={(value, country) => {
+                            setValue("mobile", value, {
+                              shouldValidate: true,
+                            });
+                            setValue("country", country);
+                          }}
+                          enableSearch
+                          countryCodeEditable={false}
+                          inputProps={{ name: "mobile" }}
+                          className="w-full [&_input]:!w-full [&_input]:!h-full h-[42px] [&_.country-list]:overflow-x-hidden"
+                        />
+                      )}
                     />
+                    {errors.mobile && (
+                      <p className="text-red-500 text-[10px] absolute -bottom-4">
+                        {errors.mobile.message}
+                      </p>
+                    )}
+                  </div>
                   </div>
                 </div>
               </div>

6a72de7a0c5c8c09bda3a64b82073f179e80b47d
Soniya <90394991+SoniyaSaini@users.noreply.github.com>
2025-10-10 18:55:37 +0530

    contact us page

--- src/components/frontend/joyOfGiving/JoyOfGiving.jsx
+++ src/components/frontend/joyOfGiving/JoyOfGiving.jsx
@@ -12,138 +12,55 @@ const JoyOfGiving = () => {
             <Image width="1356" height="452" className="w-full h-full" src="/images/joy_of_giving_banner_pc.webp" alt="" />
           </div>
         </div>
-        <div className='max-w-[90%]  mx-auto md:px-[30px] py-5'>
+        <div className='max-w-[90%]  mx-auto md:px-[34px] py-5'>
           <div>
             <div>
               <h2 className=' text-xl font-bold text-center leading-[1.6]'>Joy of Giving</h2>
               
             </div> 
-            <div className=' text-center mt-[15px]'>
-              <p className='md:text-[17px] text-base text-[#777]'>Real happiness lies in making others happy. At Flexo, we strive to spread joy by giving back to society and to Mother Nature.</p>
-              <p className='md:text-[17px] text-base text-[#777]'> As part of our Joy Of Giving initiative, we plant a fruit tree in your name for every long-term desk that you book through us.</p>
+            <div className=' text-center mt-[15px] pb-[20px]'>
+              <p className=' text-[#777]'>Real happiness lies in making others happy. At Flexo, we strive to spread joy by giving back to society and to Mother Nature.</p>
+              <p className=' text-[#777]'> As part of our Joy Of Giving initiative, we plant a fruit tree in your name for every long-term desk that you book through us.</p>
             </div>
 
           </div>
           <div className='mt-[30px] space-y-5'>
-            <div className="flex md:flex-row flex-col md:items-center items-start gap-4">
-              <div className='flex-[1.5]'>
+            <div className="flex md:flex-row flex-col md:items-center items-start gap-5.5">
+              <div className=''>
                 <Image width="700" height="466" src="/images/joy_of_giving_1.webp" alt="" />
               </div>
               <div className='flex-1'>
                 <div>
-                  <h3 className='text-[17px] font-bold'>Our Office Space Experts Help you with</h3>
+                  <h3 className='font-bold'>Helping Farmers and Rural Communities</h3>
                 </div>
                 <div className='mt-5'>
                  <p className='text-[#777]'>The trees that are planted bear fruit for about 20-25 years, benefiting farmers and rural communities for decades to come. All the trees we plant are geo-tagged, visible on google maps and can be tracked online.</p>
                 </div>
               </div>
             </div>
-            <div className="flex md:flex-row flex-col md:items-center items-start md:gap-20 gap-4 justify-between">
-              <div className='order-2 md:order-1'>
+            <div className="flex md:flex-row flex-col md:items-center items-start gap-4 justify-between">
+              <div className='order-2 md:order-1 flex-1 ps-[22px]'>
                 <div>
-                  <h3 className='text-[17px] font-bold'>Why go with FLEXO™?</h3>
+                  <h3 className=' font-bold'>Towards Sustainability</h3>
                 </div>
                 <div className='mt-5'>
-                  <ul>
-                     <li className=' leading-[30px] items-start text-[#777]'>
-                        <Svg name="smallDot" className="size-[15px] text-[#6c757d] inline"/>
-                        <span className='ps-1'>
-                          Free, fast and easy
-                        </span>
-                    </li>
-                     <li className=' leading-[30px] items-start text-[#777]'>
-                        <Svg name="smallDot" className="size-[15px] text-[#6c757d] inline"/>
-                        <span className='ps-1'>
-                         Wide range of office spaces to choose from
-                        </span>
-                    </li>
-                    <li className=' leading-[30px] items-start text-[#777]'>
-                        <Svg name="smallDot" className="size-[15px] text-[#6c757d] inline"/>
-                        <span className='ps-1'>
-                          One stop shop for customized workspace requirements
-                        </span>
-                    </li>
-
-                    <li className=' leading-[30px] items-start text-[#777]'>
-                        <Svg name="smallDot" className="size-[15px] text-[#6c757d] inline"/>
-                        <span className='ps-1'>
-                         Specialist in flexible office spaces
-                        </span>
-                    </li>
-                   <li className=' leading-[30px] items-start text-[#777]'>
-                        <Svg name="smallDot" className="size-[15px] text-[#6c757d] inline"/>
-                        <span className='ps-1'>
-                          Unbiased expert advice
-                        </span>
-                    </li>
-                    <li className=' leading-[30px] items-start text-[#777]'>
-                        <Svg name="smallDot" className="size-[15px] text-[#6c757d] inline"/>
-                        <span className='ps-1'>
-                          Give back to society and the environment. Learn more about Flexo’s  <a href="" className='text-[#0000ff]'>Joy Of Giving initiative here.</a>
-                        </span>
-                    </li>
-
-                  </ul>
+                  <p className='text-base text-[#777]'>While we can never give back what we take from Nature, we strive to make our planet more sustainable by contributing in a small but meaningful way. Planting trees helps prevent soil erosion, increases land fertility and mitigates global warming.</p>
                 </div>
               </div>
               <div className='order-1 md:order-2'>
-                <Image width="680" height="453" src="/images/enterprise-2.webp" alt="" />
+                <Image width="700" height="467" src="/images/joy_of_giving_2.webp" alt="" />
               </div>
             </div>
              <div className="flex md:flex-row flex-col md:items-center items-start gap-4">
-               <div className=''>
-                <Image width="680" height="453" src="/images/enterprise-3.webp" alt="" />
+               <div >
+                <Image width="700" height="467" src="/images/joy_of_giving_3.webp" alt="" />
               </div>
-              <div>
+              <div className='flex-1'>
                 <div>
-                  <h3 className='text-[17px] font-bold'>Benefits of Distributed Workspaces</h3>
+                  <h3 className=' font-bold'>Environment as a Stakeholder</h3>
                 </div>
                 <div className='mt-5'>
-                  <ul>
-                     <li className=' leading-[30px] items-start text-[#777]'>
-                        <Svg name="smallDot" className="size-[15px] text-[#6c757d] inline"/>
-                        <span className='ps-1'>
-                          Lower capex
-                        </span>
-                    </li>
-                     <li className=' leading-[30px] items-start text-[#777]'>
-                        <Svg name="smallDot" className="size-[15px] text-[#6c757d] inline"/>
-                        <span className='ps-1'>
-                         Flexible terms
-                        </span>
-                    </li>
-                    <li className=' leading-[30px] items-start text-[#777]'>
-                        <Svg name="smallDot" className="size-[15px] text-[#6c757d] inline"/>
-                        <span className='ps-1'>
-                          Reduced commute for employees
-                        </span>
-                    </li>
-
-                    <li className=' leading-[30px] items-start text-[#777]'>
-                        <Svg name="smallDot" className="size-[15px] text-[#6c757d] inline"/>
-                        <span className='ps-1'>
-                         Greater employee morale and retention
-                        </span>
-                    </li>
-                   <li className=' leading-[30px] items-start text-[#777]'>
-                        <Svg name="smallDot" className="size-[15px] text-[#6c757d] inline"/>
-                        <span className='ps-1'>
-                           De-risk corporate real estate strategy
-                        </span>
-                    </li>
-                    <li className=' leading-[30px] items-start text-[#777]'>
-                        <Svg name="smallDot" className="size-[15px] text-[#6c757d] inline"/>
-                        <span className='ps-1'>
-                         Easy-to-scale setups
-                        </span>
-                    </li>
-                    <li className=' leading-[30px] items-start text-[#777]'>
-                      <Svg name="smallDot" className="size-[15px] text-[#6c757d] inline"/>
-                      <span className='ps-1'>
-                       Business continuity planning
-                      </span>
-                    </li>
-                  </ul>
+                 <p className=' text-base text-[#777]'>This is a small action towards helping create a world that recognizes the environment as a stakeholder, a world that is filled with generosity and a world that gives back as it evolves. We wish to do more with your support. Let's work towards creating a better place for all of us to live in because there's no greater joy than the one there is in giving.</p>
                 </div>
               </div>
               

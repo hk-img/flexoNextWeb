310bc622f296e5bf9b9e4590d94a78d2c05e37d0
Himanshu Gandhi <162088731+himanshu-img@users.noreply.github.com>
2025-10-17 18:20:07 +0530

    added code

--- src/app/(frontend)/layout.jsx
+++ src/app/(frontend)/layout.jsx
@@ -3,6 +3,7 @@ import Header from "@/components/frontend/header/Header";
 import ChatBot from "@/components/frontend/icons/ChatBot";
 import TopLoadingBar from "@/components/frontend/topLoadingBar/TopLoadingBar";
 import FrontendProvider from "@/providers/FrontendProvider";
+import Script from "next/script";
 
 export default function RootLayout({ children }) {
   return (
@@ -13,6 +14,24 @@ export default function RootLayout({ children }) {
         {children}
         <ChatBot />
         <Footer />
+        <Script id="zoho-salesiq" strategy="afterInteractive">
+          {`
+            var $zoho = $zoho || {};
+            $zoho.salesiq = $zoho.salesiq || {
+              widgetcode: "0fc4dfe126a900d08cd66965a527bbcfebd987ea8870090a53afd7a22440aa53",
+              values: {},
+              ready: function () {}
+            };
+            var d = document;
+            s = d.createElement("script");
+            s.type = "text/javascript";
+            s.id = "zsiqscript";
+            s.defer = true;
+            s.src = "https://salesiq.zoho.in/widget";
+            t = d.getElementsByTagName("script")[0];
+            t.parentNode.insertBefore(s, t);
+          `}
+        </Script>
       </FrontendProvider>
     </>
   );

ec89b2da0a564673a040f12b2a07b0fb9c24d21e
Himanshu Gandhi <162088731+himanshu-img@users.noreply.github.com>
2025-11-26 13:32:48 +0530

    added code

--- src/app/(frontend)/layout.jsx
+++ src/app/(frontend)/layout.jsx
@@ -1,6 +1,7 @@
 import Footer from "@/components/frontend/footer/Footer";
 import Header from "@/components/frontend/header/Header";
 import ChatBot from "@/components/frontend/icons/ChatBot";
+import ScrollToTop from "@/components/frontend/scrollToTop/ScrollToTop";
 import TopLoadingBar from "@/components/frontend/topLoadingBar/TopLoadingBar";
 import FrontendProvider from "@/providers/FrontendProvider";
 import Script from "next/script";
@@ -10,6 +11,7 @@ export default function RootLayout({ children }) {
     <>
       <FrontendProvider>
         <Header />
+        <ScrollToTop/>
         <TopLoadingBar />
         {children}
         <ChatBot />
